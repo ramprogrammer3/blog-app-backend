57dd84279c081486eff2792ff307cfc89b97ebd0
ram kumar <rammaniyari@gmail.com>
2023-11-04 09:46:33 +0530

    some change

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const url = process.env.url;
 
 const connectDB = () =>{
     mongoose.connect(url,{
-        // useNewUrlParser : true,
-        // useUnifiedTopology : true,
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
     }).then(()=>{
         console.log("DB connection successful");
     }).catch((error)=>{
