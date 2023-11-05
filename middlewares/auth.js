73f90ff320277bbda0b1395d9fbcf675f1df0ec9
ram kumar <rammaniyari@gmail.com>
2023-11-05 11:35:59 +0530

    final commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -18,7 +18,7 @@ exports.auth = async(req,res,next)=>{
             // verifying token
             const decode = jwt.verify(token,'ramkumarsha256')
             req.user = decode;
-            console.log(decode)
+            // console.log(decode)
             
         } catch (error) {
             // If JWT verification fails, return 401 Unauthorized response
